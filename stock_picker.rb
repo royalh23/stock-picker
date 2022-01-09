1c352e0562b4462aa8314c7f764dc02ca5cf1e06
Royal <royalhuseyn902@gmail.com>
2022-01-09 23:33:15 +0900

    Found all the possible differences of prices

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,3 +1,19 @@
+require "pry-byebug"
+
 def stock_picker(stock_prices)
-  
+  # An array to hold the differences
+  diff = []
+
+  stock_prices.each_with_index do |price, index|
+    if price == stock_prices[-1]
+      return diff
+    end
+    stock_prices[index + 1..].each_with_index do |prc, idx|
+      dif = prc - price
+      diff << dif
+    end
+  end
+  diff
 end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
