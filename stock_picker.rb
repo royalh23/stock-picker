f51a40d42bc939610301db4d4949398f086ec82d
Royal <royalhuseyn902@gmail.com>
2022-01-09 23:45:19 +0900

    Finished the project

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,19 +1,16 @@
-require "pry-byebug"
-
 def stock_picker(stock_prices)
-  # An array to hold the differences
-  diff = []
+  # A hash to hold the differences
+  diff = {}
 
   stock_prices.each_with_index do |price, index|
     if price == stock_prices[-1]
-      return diff
+      return diff.max[1]
     end
+
     stock_prices[index + 1..].each_with_index do |prc, idx|
       dif = prc - price
-      diff << dif
-    end
+      diff[dif] = [index, stock_prices.index(prc)]
+    end 
   end
-  diff
+  diff.max[1]
 end
-
-p stock_picker([17,3,6,9,15,8,6,1,10])
